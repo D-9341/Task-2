5babd2f903c516e5e3908e3fb1c183ae60cd51aa
D-9341 <58139230+D-9341@users.noreply.github.com>
2023-10-11 15:31:28 +0300

    Delete task-10.js

--- task-10.js
+++
@@ -1,6 +0,0 @@
-const actualPrice = 4000;
-const tip = 500;
-const finalPrice = actualPrice + tip;
-const frensCount = 8;
-const toPay = finalPrice / frensCount;
-console.log(`С каждого по ${toPay} рублей, общий счёт равен ${finalPrice}, чаевые - ${tip}`)
