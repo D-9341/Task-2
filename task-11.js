c629635fe421c19ea758e8950a0eb591bda49c18
D-9341 <58139230+D-9341@users.noreply.github.com>
2023-10-11 15:50:59 +0300

    Delete task-11.js

--- task-11.js
+++
@@ -1,6 +0,0 @@
-const seconds = 100;
-const hours = Math.floor(seconds / 60 / 60);
-const minutes = Math.floor((seconds / 60) - (hours * 60));
-const sec = seconds % 60;
-const time = `${hours} ч, ${minutes} мин, ${sec} сек`
-console.log(`Время: ${time}`);
