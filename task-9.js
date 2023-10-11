563254d37e9d79f78dcb0927cbe3bae085febba6
D-9341 <58139230+D-9341@users.noreply.github.com>
2023-10-11 15:17:03 +0300

    Delete task-9.js

--- task-9.js
+++
@@ -1,5 +0,0 @@
-const sum = 200000;
-const years = 2;
-const percent = 10;
-const overPricing = sum / percent * years
-console.log(`Сумма выплаты: ${overPricing + sum}\nСумма переплаты: ${overPricing}`)
